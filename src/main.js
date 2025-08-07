250c65dbea950717753ce35cf6385e254f8c4e66
jominhyeong97 <you@example.com>
2025-08-07 18:39:20 +0900

    8월7일

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+// 중괄호가 쳐져있으면 export된 항목 중 하나만 선택하여 import
+// 중괄호가 없으면 default(대표요소)를 import
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')

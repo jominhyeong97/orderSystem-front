2d06c9ac60893313be64c43944a50d65d6cca9d3
jominhyeong97 <you@example.com>
2025-08-12 16:57:48 +0900

    검색기능 및 페이징처리 구현

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,12 @@ import { createRouter, createWebHistory } from "vue-router"
 import { practiceRouter } from "./practiceRouter";
 import { memberRouter } from "./memberRouter";
 import { orderRouter } from "./orderRouter";
+import { productRouter } from "./productRouter";
 
 
 // routes에서 url 경로와 화면을 매핑
 const routes = [
-   ...practiceRouter, ...memberRouter, ...orderRouter
+   ...practiceRouter, ...memberRouter, ...orderRouter, ...productRouter
 
 ]
 
